f4e667e5430119ef6ed6e6069cebcc9f3eb49c13
fernandogutz <03fernandogutierrez@gmail.com>
2023-05-10 23:38:59 -0400

    API Changed

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,7 +1,7 @@
 
 const fetchData = async(id) => {
 
-    return fetch(`https://blog.fernandogutz.com/wp-json/wp/v2/posts/${id}`, { next: {revalidate: 3} }) //{cache:'no-store'} = Para SSR || {next: {revalidate: 60}} = Para ISR || Dejar vacío para SSG, por defecto
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { next: {revalidate: 3} }) //{cache:'no-store'} = Para SSR || {next: {revalidate: 60}} = Para ISR || Dejar vacío para SSG, por defecto
         .then(resp => resp.json())
 
 }
@@ -12,8 +12,8 @@ const PostPage = async ({ params }) => {
 
     return (
         <>
-            <h1>{post.title.rendered}</h1>
-            <p>{post.content.rendered}</p>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
         </>
     )
 }
--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -2,7 +2,7 @@ import LikeButton from "@/components/LikeButton";
 import Link from "next/link";
 
 const fetchData = () => {
-    return fetch('https://blog.fernandogutz.com/wp-json/wp/v2/posts', {cache: 'no-store'}) //{cache:'no-store'} = Para SSR || {next: {revalidate: 60}} = Para ISR || Dejar vacío para SSG, por defecto
+    return fetch('https://jsonplaceholder.typicode.com/posts', {cache: 'no-store'}) //{cache:'no-store'} = Para SSR || {next: {revalidate: 60}} = Para ISR || Dejar vacío para SSG, por defecto
         .then(resp => resp.json())
 
 }
@@ -17,8 +17,8 @@ const PostsPage = async () => {
             {
                 posts.map(post => ( // no olvidar de usar paréntesis en vez de llaves jajajja siempre hago lo mismo xd
                     <article key={post.id}>
-                        <h3>{post.title.rendered}</h3>
-                        <p>{post.excerpt.rendered.slice(3, 120)}...</p>
+                        <h3>{post.title}</h3>
+                        <p>{post.body.slice(3, 120)}...</p>
                         <Link href={`/posts/${post.id}`}>Leer más</Link>
                         <LikeButton id={post.id}></LikeButton>
                     </article>
